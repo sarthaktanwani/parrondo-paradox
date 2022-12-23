64b30042d5fab33bc7b12f6358162ac7105f9340
Sarthak <stanwani7@gmail.com>
2022-12-23 12:22:31 +0530

    code reorganization, and paradox working again

--- main.c
+++ main.c
@@ -4,15 +4,29 @@
 #include <time.h>       //for srand(time(NULL))
 #include <termios.h>    //to immediately respond to keystrokes on the terminal
 
-int gameA(void);
-int gameB(int);
-int gameC(int);
+#define NUMBER_OF_GAMES 3
 
-void printStats(int, int, int, int);
+typedef struct
+{
+    int wins[NUMBER_OF_GAMES], losses[NUMBER_OF_GAMES], gameBal[NUMBER_OF_GAMES];
+}Stats;
+
+int gameA(Stats *s);
+int gameB(Stats *s);
+int gameC(Stats *s);
+
+// void printStats(int, int, int, int);
+
+
+void printStats(int iter, Stats *);
+void resetStats(Stats *);
 
 int main()
 {
     int iter = 0;
+    Stats s1;
+    resetStats(&s1);
+    
     int balGameA = 0,balGameB = 0, balGameC = 0;
     int winsA = 0, winsB = 0, winsC = 0;
     int lossesA = 0, lossesB = 0, lossesC = 0;
@@ -29,7 +43,8 @@ int main()
     srand(time(NULL));
 
 
-    while(balGameC <= 0)
+    while(s1.gameBal[2] <= 0)
+    // while(balGameC <= 0) 
     {
         // float random_number = (float)rand() / (float) RAND_MAX;
         // printf("random number 1 is: %f\n", random_number);
@@ -38,51 +53,65 @@ int main()
         // printf("Enter the number of iterations: ");
         // scanf(" %d", &iter);
         // printf("iterations are: %d\n", iter);
+        resetStats(&s1);
+        winsA = winsB = winsC = lossesA = lossesB = lossesC = 0;
         balGameA = balGameB = balGameC = 0;
         iter = 1000;
         for(int i = 0; i < iter; i++)
         {
-            int res = gameA();
+            int res = gameA(&s1);
             if(res == 0)
             {
                 balGameA--;
                 lossesA++;
+                // s1.gameBal[0]--;
+                // s1.losses[0]++;
             }
             else
             {
                 balGameA++;
                 winsA++;
+                // s1.gameBal[0]++;
+                // s1.wins[0]++;
             }
 
-            res = gameB(balGameB);
+            res = gameB(&s1);
             if(res == 0)
             {
                 balGameB--;
                 lossesB++;
+                // s1.gameBal[1]--;
+                // s1.losses[1]++;
             }
             else
             {
                 balGameB++;
                 winsB++;
+                // s1.gameBal[1]++;
+                // s1.wins[1]++;
             }
-
-
-            res = gameC(balGameC);
+            res = gameC(&s1);
             if(res == 0)
             {
                 balGameC--;
                 lossesC++;
+                // s1.gameBal[2]--;
+                // s1.losses[2]++;
             }
             else
             {
                 balGameC++;
                 winsC++;
+                // s1.gameBal[2]++;
+                // s1.wins[2]++;
             }
         }
 
-        printStats(iter, winsA, winsB, winsC);
+        printStats(iter, &s1);
 
         printf("iterations complete\n");
+        // printf("gameBalC = %d, finalgameBal: %d\n", balGameC, s1.gameBal[2]);
+        // printf("iterations complete\n");
 
         // printf("Enter the prompt: ");
 
@@ -99,67 +128,80 @@ int main()
         // {
         //     break;
         // }
-        winsA = winsB = winsC = lossesA = lossesB = lossesC = 0;
+        // resetStats(&s1);  
     }
 
     printf("Hello World\n");
     return 0;
 }
 
-void printStats(int iter, int winsA, int winsB, int winsC)
+void resetStats(Stats *s)
 {
-    int lossA = iter - winsA;
-    int lossB = iter - winsB;
-    int lossC = iter - winsC;
-    int balA = winsA - lossA, balB = winsB - lossB, balC = winsC - lossC;
-    
+    for(int i = 0; i < NUMBER_OF_GAMES; i++)
+    {
+        s->wins[i] = 0;
+        s->losses[i] = 0;
+        s->gameBal[i] = 0;
+    }
+}
+
+void printStats(int iter, Stats *s)
+{    
     printf("******Game A Stats******\n");
-    printf("Final Balance: %d\n", balA);
-    printf("Wins: %d/%d\tLosses: %d/%d\t\n", winsA, iter, lossA, iter);
-    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)winsA/iter*100, (float)lossA/iter*100);
+    printf("Final Balance: %d\n", s->gameBal[0]);
+    printf("Wins: %d/%d\tLosses: %d/%d\t\n", s->wins[0], iter, s->losses[0], iter);
+    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)s->wins[0]/iter*100, (float)s->losses[0]/iter*100);
 
     printf("******Game B Stats******\n");
-    printf("Final Balance: %d\n", balB);
-    printf("Wins: %d/%d\tLosses: %d/%d\t\n", winsB, iter, lossB, iter);
-    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)winsB/iter*100, (float)lossB/iter*100);
+    printf("Final Balance: %d\n", s->gameBal[1]);
+    printf("Wins: %d/%d\tLosses: %d/%d\t\n", s->wins[1], iter, s->losses[1], iter);
+    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)s->wins[1]/iter*100, (float)s->losses[1]/iter*100);
 
     printf("******Game C Stats******\n");
-    printf("Final Balance: %d\n", balC);
-    printf("Wins: %d/%d\tLosses: %d/%d\t\n", winsC, iter, lossC, iter);
-    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)winsC/iter*100, (float)lossC/iter*100);
+    printf("Final Balance: %d\n", s->gameBal[2]);
+    printf("Wins: %d/%d\tLosses: %d/%d\t\n", s->wins[2], iter, s->losses[2], iter);
+    printf("%% Wins: %0.2f%%\tLosses: %0.2f%%\t\n\n", (float)s->wins[2]/iter*100, (float)s->losses[2]/iter*100);
 }
 
 
-int gameA(void)
+int gameA(Stats *s)
 {
     float random_number = (float)rand() / (float) RAND_MAX;
 
     if(random_number < 0.495)
     {
         //Win with a chance of 49.5%
+        s->wins[0]++;
+        s->gameBal[0]++;
         return 1;
     }
     else
     {
         //Lose with a chance of 50.5%
+        s->losses[0]++;
+        s->gameBal[0]--;
         return 0;
     }
 }
 
-int gameB(int balance)
+int gameB(Stats *s)
 {
     float random_number = (float)rand() / (float) RAND_MAX;
 
-    if(balance % 3 == 0)
+    if(s->gameBal[1] % 3 == 0)
     {
         if(random_number < 0.01)
         {
             //win with a 1% chance
+            s->wins[1]++;
+            s->gameBal[1]++;
             return 1;
         }
         else
         {
             //lose with a 99.9% chance
+            s->losses[1]++;
+            s->gameBal[1]--;
             return 0;
         }
     }
@@ -168,18 +210,22 @@ int gameB(int balance)
         if(random_number < 0.75)
         {
             //Win with a 75% chance
+            s->wins[1]++;
+            s->gameBal[1]++;
             return 1;
         }
         else
         {
             //lose with a 25% chance
+            s->losses[1]++;
+            s->gameBal[1]--;
             return 0;
         }
     }
 }
 
 
-int gameC(int balance)
+int gameC(Stats *s)
 {
     float random_number = (float)rand() / (float) RAND_MAX;
     (void)random_number;
@@ -195,14 +241,64 @@ int gameC(int balance)
     
     if(random_number > 0.5)
     {
-        return gameA();
+        random_number = (float)rand() / (float) RAND_MAX;
+
+        if(random_number < 0.495)
+        {
+            //Win with a chance of 49.5%
+            s->wins[2]++;
+            s->gameBal[2]++;
+            return 1;
+        }
+        else
+        {
+            //Lose with a chance of 50.5%
+            s->losses[2]++;
+            s->gameBal[2]--;
+            return 0;
+        } 
     }
     else if(random_number < 0.5)
     {
-        return gameB(balance);
+        random_number = (float)rand() / (float) RAND_MAX;
+
+        if(s->gameBal[2] % 3 == 0)
+        {
+            if(random_number < 0.01)
+            {
+                //win with a 1% chance
+                s->wins[2]++;
+                s->gameBal[2]++;
+                return 1;
+            }
+            else
+            {
+                //lose with a 99.9% chance
+                s->losses[2]++;
+                s->gameBal[2]--;
+                return 0;
+            }
+        }
+        else
+        {
+            if(random_number < 0.75)
+            {
+                //Win with a 75% chance
+                s->wins[2]++;
+                s->gameBal[2]++;
+                return 1;
+            }
+            else
+            {
+                //lose with a 25% chance
+                s->losses[2]++;
+                s->gameBal[2]--;
+                return 0;
+            }
+        }
     }
     else
     {
-        return gameC(balance);
+        return gameC(s);
     }
 }
